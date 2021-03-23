a2d7fe381816dcd10aedac849cf0c22865ce67ac
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2021-03-23 12:10:48 +0100

    Use classes to generate the nanoindentation scheme

--- MetaDataFiles/TurtleSchemeGenerator/gen_nano_ind_schemes.py
+++ MetaDataFiles/TurtleSchemeGenerator/gen_nano_ind_schemes.py
@@ -1,112 +1,91 @@
-from data_scheme import MetaDataSchemes as Scheme, FieldList
-from data_scheme import DropdownList, SFBFields, MetaDataField
+from data_scheme import MetaDataSchemes, SFBFields
 
-nano_ind_blue = FieldList()
-nano_ind_blue.add(label="Experiment ID", required=True)
-nano_ind_blue.add(label="Operator", required=True)
-nano_ind_blue.add(label="Specimen ID", sh_path="csmd:investigation_sample")
-nano_ind_blue.add(label="Parent sample specimen ID", name="parentSample")
-nano_ind_blue.add(label="Sample Location")
-nano_ind_blue.add(label="Instrument used", name="instrument")
-nano_ind_blue.add(label="Tip used", name="tip")
-nano_ind_blue.add(label="Test location on sample")
-nano_ind_blue.add(label="Type of test")
-nano_ind_blue.add(label="Control method")
-nano_ind_blue.add(label="Tip ID")
-nano_ind_blue.add(label="Diamond area function")
-nano_ind_blue.add(label="Date of Calibration", field_type="date")
-nano_ind_blue.add(label="Frame stiffness", unit='N/m', other_relations={"qudt:Unit": "unit:N-PER-M"})
-nano_ind_blue.add(label="Comments", long=True)
 
-nano_ind_grey = FieldList()
-nano_ind_grey.add(label="Relative Humidity", unit='%', other_relations={"qudt:Unit": "unit:PERCENT_RH"})
-nano_ind_grey.add(label="Environmental protection during specimen testing", name="TestingEnv")
-nano_ind_grey.add(label="Environmental gas")
-nano_ind_grey.add(label="Test date", field_type="date")
+class PreparationRoutine(SFBFields):
+    def __init__(self):
+        super().__init__()
+        # yellow fields
+        self.add(label="Preparation routine", long=True)
+        self.add(label="Sample storage")
+        self.add(label="Preparation Date", field_type="date", long=True)
+        self.add(label="Etching routine")
+        self.add(label="Sample Orientation", long=True)
+        self.add(label="Experiment IDs of other tests performed on the same specimen", long=True)
+        self.add(label='Any data set to be linked with this experiment', long=True)
 
-nano_ind_yellow = FieldList()
-nano_ind_yellow.add(label="Preparation routine", long=True)
-nano_ind_yellow.add(label="Sample storage")
-nano_ind_yellow.add(label="Preparation Date", field_type="date", long=True)
-nano_ind_yellow.add(label="Etching routine")
-nano_ind_yellow.add(label="Sample Orientation", long=True)
-nano_ind_yellow.add(label="Experiment IDs of other tests performed on the same specimen", long=True)
-nano_ind_yellow.add(label='Any data set to be linked with this experiment', long=True)
 
-nano_ind_green = FieldList()
-nano_ind_green.add(label="Target load", unit="mN", other_relations={"qudt:Unit": "unit:MilliN"})
-nano_ind_green.add(label="Target depth", unit="nm", other_relations={"qudt:Unit": "unit:NanoM"})
-nano_ind_green.add(label="Target strain rate", unit='/s', other_relations={"qudt:Unit": "unit:PER-SEC"})
-nano_ind_green.add(label="Target loading rate", unit="mN/s", other_relations={"qudt:Unit": "unit:MilliN-PER-SEC"})
-nano_ind_green.add(label="Target displacement rate", unit="nm/s", other_relations={"qudt:Unit": "unit:NanoM-PER-SEC"})
-nano_ind_green.add(label="Continuous stiffness measurement", field_type='bool')
-nano_ind_green.add(label="Start of averaging depth", unit="nm", other_relations={"qudt:Unit": "unit:NanoM"})
-nano_ind_green.add(label="End of averaging depth", unit="nm", other_relations={"qudt:Unit": "unit:NanoM"})
-nano_ind_green.add(label="Hold time at maximum load", unit='s', other_relations={"qudt:Unit": "unit:SEC"})
-nano_ind_green.add(label="Drift correction enabled", field_type="bool")
-nano_ind_green.add(label="Sample temperature", unit="\u00b0C", other_relations={"qudt:Unit": "unit:DEG_C"})
-nano_ind_green.add(label="Tip temperature", unit="\u00b0C", other_relations={"qudt:Unit": "unit:DEG_C"})
+class NanoIndentationBasic(PreparationRoutine):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Specimen ID", sh_path="csmd:investigation_sample", order_priority=2)
+        self.add(label="Parent sample specimen ID", name="parentSample", order_priority=2)
+        self.add(label="Sample Location", order_priority=2)
+        self.add(label="Instrument used", name="instrument", sh_path="csmd:investigation_instrument")
+        self.add(label="Tip used", name="tip")
+        self.add(label="Test location on sample")
+        self.add(label="Type of test")
+        self.add(label="Control method")
+        self.add(label="Tip ID")
+        # grey fields (with negative priority to have them at the end of the scheme)
+        self.add(label="Relative Humidity", unit='%', other_ttl_relations={"qudt:Unit": "unit:PERCENT_RH"},
+                 order_priority=-50)
+        self.add(label="Environmental protection during specimen testing", name="TestingEnv", order_priority=-50)
+        self.add(label="Environmental gas", order_priority=-50)
+        self.add(label="Test date", field_type="date", order_priority=-50)
 
-nano_ind_green_SRJ = FieldList()
-nano_ind_green_SRJ.add(label="Target load", unit="mN", other_relations={"qudt:Unit": "unit:MilliN"})
-nano_ind_green_SRJ.add(label="Target depth", unit="nm", other_relations={"qudt:Unit": "unit:NanoM"})
-nano_ind_green_SRJ.add(label="Initial strain rate", unit='/s', other_relations={"qudt:Unit": "unit:PER-SEC"})
-nano_ind_green_SRJ.add(label="Initial displacement", unit="nm", other_relations={"qudt:Unit": "unit:NanoM"})
-nano_ind_green_SRJ.add(label="No. of cycles", name="NoOfCycles")
-nano_ind_green_SRJ.add(label="Final strain rate", unit='/s', other_relations={"qudt:Unit": "unit:PER-SEC"})
-nano_ind_green_SRJ.add(label="Displacement range for strain rate ", unit="nm", other_relations={"qudt:Unit": "unit:NanoM"})
-nano_ind_green_SRJ.add(label="Continuous stiffness measurement", field_type='bool')
-nano_ind_green_SRJ.add(label="Drift correction enabled", field_type="bool")
-nano_ind_green_SRJ.add(label="Sample temperature", unit="\u00b0C", other_relations={"qudt:Unit": "unit:DEG_C"})
-nano_ind_green_SRJ.add(label="Tip temperature", unit="\u00b0C", other_relations={"qudt:Unit": "unit:DEG_C"})
 
-nano_ind = Scheme("NanoIndentation")
-nano_ind.fields = nano_ind_blue
-nano_ind.write("Nano-intendation.ttl")
+class NanoIndentation(NanoIndentationBasic):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Target load", unit="mN")
+        self.add(label="Target depth", unit="nm")
+        self.add(label="Target strain rate", unit='/s')
+        self.add(label="Target loading rate", unit="mN/s")
+        self.add(label="Target displacement rate", unit="nm/s")
+        self.add(label="Continuous stiffness measurement", field_type='bool')
+        self.add(label="Start of averaging depth", unit="nm")
+        self.add(label="End of averaging depth", unit="nm")
+        self.add(label="Hold time at maximum load", unit='s')
+        self.add(label="Drift correction enabled", field_type="bool")
+        self.add(label="Sample temperature", unit="\u00b0C")
+        self.add(label="Tip temperature", unit="\u00b0C")
+        # Listed later in case of creep test in the docx
+        self.add(label="Diamond area function")
+        self.add(label="Date of Calibration", field_type="date")
+        self.add(label="Frame stiffness", unit='N/m')
 
-nano_ind.fields = nano_ind_grey
-nano_ind.write("Nano-intendation_gray.ttl")
+        self.sort_fields_by_order_priority()
 
-nano_ind.fields = nano_ind_green
-nano_ind.write("Nano-intendation_green.ttl")
 
-nano_ind.fields = nano_ind_yellow
-nano_ind.write("Nano-intendation_yellow.ttl")
+class NanoIndentationSRJ(NanoIndentationBasic):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Target load", unit="mN")
+        self.add(label="Target depth", unit="nm")
+        self.add(label="Initial strain rate", unit='/s')
+        self.add(label="Initial displacement", unit="nm")
+        self.add(label="No. of cycles", name="NoOfCycles")
+        self.add(label="Final strain rate", unit='/s')
+        self.add(label="Displacement range for strain rate ", unit="nm")
+        self.add(label="Continuous stiffness measurement", field_type='bool')
+        self.add(label="Drift correction enabled", field_type="bool")
+        self.add(label="Sample temperature", unit="\u00b0C")
+        self.add(label="Tip temperature", unit="\u00b0C")
 
-nano_ind.fields = nano_ind_blue + nano_ind_grey + nano_ind_green + nano_ind_yellow
-nano_ind.write("Nano-intendation_full.ttl")
+        self.sort_fields_by_order_priority()
 
-basic_scheme = Scheme("NanoIndentation/basic")
-basic_scheme.fields = SFBFields(id_name="Experiment ID")
 
-# Missing blue fields:
-nano_ind_blue = FieldList()
-nano_ind_blue.add(label="Specimen ID", sh_path="csmd:investigation_sample", order_priority=2)
-nano_ind_blue.add(label="Parent sample specimen ID", name="parentSample", order_priority=2)
-nano_ind_blue.add(label="Sample Location", order_priority=2)
-nano_ind_blue.add(label="Instrument used", name="instrument", sh_path="csmd:investigation_instrument")
-nano_ind_blue.add(label="Tip used", name="tip")
-nano_ind_blue.add(label="Test location on sample")
-nano_ind_blue.add(label="Type of test")
-nano_ind_blue.add(label="Control method")
-nano_ind_blue.add(label="Tip ID")
+class NanoIndentationCreep(NanoIndentationBasic):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Creep dwell period", unit='s')
 
-nano_ind_blue_creep = FieldList()
-nano_ind_blue_creep.add(label="Creep dwell period", unit='s')
 
-nano_ind_blue2 = FieldList()
-nano_ind_blue2.add(label="Diamond area function")
-nano_ind_blue2.add(label="Date of Calibration", field_type="date")
-nano_ind_blue2.add(label="Frame stiffness", unit='N/m', other_relations={"qudt:Unit": "unit:N-PER-M"})
-
-sample_origin_scheme = Scheme("NanoIndentation/SampleOrigin", extends=basic_scheme)
-sample_origin_scheme.fields = nano_ind_yellow
-
-nano_ind_scheme = Scheme("NanoIndentation", extends=sample_origin_scheme)
-nano_ind_scheme.order_fields_by_priority = True
-nano_ind_scheme.fields = nano_ind_blue + nano_ind_blue2 + nano_ind_green + nano_ind_grey
-
+nano_ind_scheme = MetaDataSchemes("NanoIndentation")
+nano_ind_scheme.fields = NanoIndentation()
 nano_ind_scheme.write()
+
+nano_ind_scheme.fields.write('NanoIndentation.txt')
 
 with open("nano_indentation_additional_terms.ttl", 'w', encoding='utf8') as f:
     for field in nano_ind_scheme.full_field_list:
