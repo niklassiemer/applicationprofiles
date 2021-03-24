a82c76a128ba2be3dbe218d4259ff131b2ae5007
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2021-03-24 22:04:24 +0100

    Use classes in DAMASK scheme generation

--- MetaDataFiles/TurtleSchemeGenerator/gen_DAMASK_schemes.py
+++ MetaDataFiles/TurtleSchemeGenerator/gen_DAMASK_schemes.py
@@ -1,64 +1,82 @@
 from data_scheme import MetaDataSchemes as Scheme
-from data_scheme import MetaDataField as Field
+from data_scheme import FieldList, SFBFields
 
 micro = "\u03bc"
 deg = "\u00b0"
+squared = '\u00B2'
 
-blue = [
-    Field(label="Operator"),
-    Field(label='Affiliation'),
-    Field(label="Simulation ID/name", name='simulationID'),
-    Field(label="Constitutive models"),
-    Field(label="Library module"),
-    Field(label="Software version"),
-    Field(label="Comment", long=True)
-]
 
-green = [
-    # Material input
-    Field(label="Plasticity model"),
-    Field(label="Lattice structure"),
-    Field(label="Elastic moduli", unit='Pa'),
-    Field(label="Slip families"),
-    Field(label="Initial dislocation density", unit='1/m**2'),
-    Field(label="Peierls stress", unit='Pa'),
-    # Geometry input
-    Field(label="Physical size of the geometry", unit='m'),
-    Field(label="Grids of the geometry"),
-    # Boundary conditions
-    Field(label="Deformation gradient rate"),
-    Field(label="Applied stress", unit='Pa'),
-    Field(label="Simulation time", unit='s'),
-    Field(label="Numerical increments"),
-    # Numerical input
-    Field(label="Mechanical solver"),
-    Field(label="Phase field solver"),
-    Field(label="Maximum iterations"),
-    Field(label="Maximum cutbacks")
-]
+class DamaskBasic(SFBFields):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Constitutive models")
+        self.add(label="Library module")
+        self.add(label="Software version")
 
-grey = [
-    Field(label="Cluster name"),
-    Field(label='CPU info'),
-    Field(label='cores'),
-    Field(label='Required RAM', unit='GB'),
-    Field(label="Runtime", unit='h'),
-    Field(label="Submission time"),
-    Field(label="Stop time")
-]
 
+class DamaskMaterial(FieldList):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Plasticity model")
+        self.add(label="Lattice structure")
+        self.add(label="Elastic moduli", unit='Pa')
+        self.add(label="Slip families")
+        self.add(label="Initial dislocation density", unit='1/m'+squared)
+        self.add(label="Peierls stress", unit='Pa')
+
+
+class DamaskGeometry(FieldList):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Physical size of the geometry", unit='m')
+        self.add(label="Grids of the geometry")
+
+
+class DamaskBoundaryConditions(FieldList):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Deformation gradient rate")
+        self.add(label="Applied stress", unit='Pa')
+        self.add(label="Simulation time", unit='s')
+        self.add(label="Numerical increments")
+
+class DamaskNumerical(FieldList):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Mechanical solver")
+        self.add(label="Phase field solver")
+        self.add(label="Maximum iterations")
+        self.add(label="Maximum cutbacks")
+
+
+class DamaskGrey(FieldList):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Cluster name")
+        self.add(label='CPU info')
+        self.add(label='cores')
+        self.add(label='Required RAM', unit='GB')
+        self.add(label="Runtime", unit='h')
+        self.add(label="Submission time")
+        self.add(label="Stop time")
+
+
+class DamaskCoScInE(DamaskGrey, DamaskBasic):
+    def __init__(self):
+        super().__init__()
+        self.sort_fields_by_order_priority()
+
+
+class Damask(DamaskGrey, DamaskNumerical, DamaskBoundaryConditions, DamaskGeometry, DamaskMaterial, DamaskBasic):
+    def __init__(self):
+        super().__init__()
+        self.sort_fields_by_order_priority()
 
 
 DAMASK = Scheme("DAMASK")
-DAMASK.fields = blue
+DAMASK.fields = DamaskCoScInE()
 DAMASK.write()
 
-DAMASK.fields = green
-DAMASK.write("DAMASK_green.ttl")
-
-DAMASK.fields = grey
-DAMASK.write("DAMASK_grey.ttl")
-
-DAMASK.fields = blue + green + grey
+DAMASK.fields = Damask()
 DAMASK.write("DAMASK_full.ttl")
 
