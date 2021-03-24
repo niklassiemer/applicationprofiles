d27660bfaa7cacd9dfedaa2559184ae18a25d71e
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2021-03-24 23:38:01 +0100

    Use classes in LightMicroscope scheme generation

--- MetaDataFiles/TurtleSchemeGenerator/gen_light_micro_schemes.py
+++ MetaDataFiles/TurtleSchemeGenerator/gen_light_micro_schemes.py
@@ -1,36 +1,50 @@
 from data_scheme import MetaDataSchemes as Scheme
 from data_scheme import MetaDataField as Field
+from data_scheme import SFBFields, FieldList
 
 micro = "\u03bc"
 deg = "\u00b0"
 
-blue = [
-    Field(label="Operator"),
-    Field(label="Experiment ID"),
-    Field(label="Date of preparation", field_type='date'),
-    Field(label='Sample storage'),
-    Field(label="Pre-treatment"),
-    Field(label="Instrument used"),
-    Field(label="Position sample"),
-    Field(label="Filter"),
-    Field(label="Magnification"),
-    Field(label="Scale", unit=micro+'m/px')
-]
 
-yellow = [
-    Field(label="Parent sample specimen ID"),
-    Field(label="Specimen ID"),
-    Field(label="Preparation routine"),
-    Field(label="Immersion Experiment ID")
-]
+class LightMicroscopeBasic(SFBFields):
+    def __init__(self):
+        super().__init__()
+        self._fields += [
+            Field(label="Date of preparation", field_type='date'),
+            Field(label='Sample storage'),
+            Field(label="Pre-treatment"),
+            Field(label="Instrument used"),
+            Field(label="Position sample"),
+            Field(label="Filter"),
+            Field(label="Magnification"),
+            Field(label="Scale", unit=micro+'m/px', qudt='MicroM-PER-NUM')
+        ]
 
-LightMicro = Scheme("LightMicroscope")
-LightMicro.fields = blue
-LightMicro.write()
 
-LightMicro.fields = yellow
-LightMicro.write("LightMicroscope_yellow.ttl")
+class Yellow(FieldList):
+    def __init__(self):
+        super().__init__()
+        self._fields += [
+            Field(label="Parent sample specimen ID"),
+            Field(label="Specimen ID"),
+            Field(label="Preparation routine"),
+            Field(label="Immersion Experiment ID")
+        ]
 
-LightMicro.fields = blue + yellow
-LightMicro.write("LightMicroscope_full.ttl")
+
+class LightMicroscope(Yellow, LightMicroscopeBasic):
+    def __init__(self):
+        super().__init__()
+        self.sort_fields_by_order_priority()
+
+
+LightMicro_scheme = Scheme("LightMicroscope")
+LightMicro_scheme.fields = LightMicroscopeBasic()
+LightMicro_scheme.write()
+
+LightMicro_scheme.fields = Yellow()
+LightMicro_scheme.write("LightMicroscope_yellow.ttl")
+
+LightMicro_scheme.fields = LightMicroscope()
+LightMicro_scheme.write("LightMicroscope_full.ttl")
 
