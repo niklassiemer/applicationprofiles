852b78e30c471dfb7e6bd37dcf10859315733801
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2021-03-24 21:24:18 +0100

    Use classes in atomistic schemes generation

--- MetaDataFiles/TurtleSchemeGenerator/gen_atomistic_schemes.py
+++ MetaDataFiles/TurtleSchemeGenerator/gen_atomistic_schemes.py
@@ -1,246 +1,321 @@
 from data_scheme import MetaDataSchemes as Scheme, FieldList, SFBFields
 
 angstrom = "\u212B"
+squared = '\u00B2'
+cubic = '\u00B3'
 
-sample_blue = SFBFields()
-sample_blue.add(label="Parent ID")
 
-sample_yellow = FieldList()
-sample_yellow.add(label="Generator ID")
-sample_yellow.add(label="Generator frame")  # int
-sample_yellow.add(label="Construction method", long=True)  # i.e. pyiron command
+class SampleBasic(SFBFields):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Parent ID")
 
-sample_green = FieldList()
-sample_green.add(label="Chemical formula")  # str
-sample_green.add(label="Chemical species")  # list(str)
-sample_green.add(label="Number of atoms")  # int
-sample_green.add(label="Chemical species count")  # dict
 
-sample_grey = FieldList()
-sample_grey.add(label="Defects contained")
-sample_grey.add(label="Mechanical treatment")
-sample_grey.add(label="Heat treatment")  # aka temperature
-sample_grey.add(label="Point group")
-sample_grey.add(label="Sublattices")
+class SampleCoScInE(SampleBasic):
+    def __init__(self):
+        super().__init__()
+        # yellow
+        self.add(label="Generator ID")
+        self.add(label="Generator frame")  # int
+        self.add(label="Construction method", long=True)  # i.e. pyiron command
+        # grey
+        self.add(label="Defects contained")
+        self.add(label="Mechanical treatment")
+        self.add(label="Heat treatment")  # aka temperature
+        self.add(label="Point group")
+        self.add(label="Sublattices")
+
+        self.sort_fields_by_order_priority()
+
+
+class Sample(SampleCoScInE):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Chemical formula")  # str
+        self.add(label="Chemical species")  # list(str)
+        self.add(label="Number of atoms")  # int
+        self.add(label="Chemical species count")  # dict
+
+        self.sort_fields_by_order_priority()
+
 
 sample_scheme = Scheme("AtomisticSample")
-sample_scheme.fields = sample_blue + sample_yellow + sample_green + sample_grey
-sample_scheme.write("AtomisticSample_full.ttl")
-
-sample_scheme.fields = sample_blue
-sample_scheme.write()
-
-sample_scheme.fields = sample_grey
-sample_scheme.write("AtomisticSample_grey.ttl")
-
-sample_scheme.fields = sample_green
-sample_scheme.write("AtomisticSample_green.ttl")
-
-sample_scheme.fields = sample_yellow
-sample_scheme.write("AtomisticSample_yellow.ttl")
+sample_scheme.fields = SampleCoScInE()
+sample_scheme.fields.write('AtomisticSample.txt')
+sample_scheme.write("AtomisticSample.ttl")
 
 
-sim_blue = SFBFields()
-# TODO: discuss: This is listed in green for simulations!
-#sim_blue.add(label="Sample ID", required=True)
-sim_blue.add(label="Status", field_type="list")  # Choice ('initialized', 'running', etc)
-sim_blue.add(label="Last status update")  # timestamp
-sim_blue.add(label="pyiron version")
-sim_blue.add(label="Other software versions")
-sim_blue.add(label="External ID")
-sim_blue.add(label="Simulation type")
-# We have DOI and References, DOI is for papers about this and references for what?!
-sim_blue.add(label="References")
+class SimBasic(SFBFields):
+    # TODO: discuss: This is listed in green for simulations!
+    # sim_blue.add(label="Sample ID", required=True)
+    def __init__(self):
+        super().__init__()
+        self.add(label="Status", field_type="list")  # Choice ('initialized', 'running', etc)
+        self.add(label="Last status update")  # timestamp
+        self.add(label="pyiron version")
+        self.add(label="Other software versions")
+        self.add(label="External ID")
+        self.add(label="Simulation type")
+        # We have DOI and References, DOI is for papers about this and references for what?!
+        self.add(label="References")
 
-sim_technical = FieldList()
-sim_technical.add(label="Cluster")
-sim_technical.add(label="Node")
-sim_technical.add(label="Cores")
-sim_technical.add(label="Runtime")
-sim_technical.add(label="Submission time")
-sim_technical.add(label="Stop time")
 
-sim_universal = sim_blue + sim_technical
-# Since I took it out of the blue:
-sim_universal.add(label="Sample ID") #, required=True)
+class SimTechnical(FieldList):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Cluster")
+        self.add(label="Node")
+        self.add(label="Cores")
+        self.add(label="Runtime")
+        self.add(label="Submission time")
+        self.add(label="Stop time")
 
-sim_lammps = FieldList()
-sim_lammps.add(label="Potential")
-sim_lammps.add(label="Lammps version")
 
-sim_vasp = FieldList()
-sim_vasp.add(label="Plane wave energy cutoff", unit='eV')
-sim_vasp.add(label="Kpoint mesh")
-sim_vasp.add(label="Pseudopotential")
-sim_vasp.add(label="Spin polarization", field_type="bool")
-# ...etc?
+# Inherit from SimTechnical first since then its fields are added to the SimBasic.
+class SimUniversal(SimTechnical, SimBasic):
+    def __init__(self):
+        super().__init__()
+        # Since I took it out of the blue:
+        self.add(label="Sample ID") #, required=True)
 
-sim_md = FieldList()
-sim_md.add(label="Temperature", unit='K')
-sim_md.add(label="Thermodynamic ensemble")
-sim_md.add(label="Initial temperature", unit='K')  # For initial kinetic energy
-sim_md.add(label="Temperature end", unit='K')  # For linear ramping between Temperature and this value
-sim_md.add(label="Thermostat style", field_type="list")
-sim_md.add(label="Thermostat timescale", unit='fs')
-sim_md.add(label="Pressure", unit="GPa")
-sim_md.add(label="Pressure end", unit="GPa")
-sim_md.add(label="Barostat style", field_type="list")
-sim_md.add(label="Barostat timescale", unit="fs")
-sim_md.add(label="Time step", unit='fs')
-sim_md.add(label="Atomic steps")
-sim_md.add(label="Print period")
-sim_md.add(label="Print detail")
-sim_md.add(label="Random seed")
 
-sim_minimize = FieldList()
-sim_minimize.add(label="Force convergence criterion", unit='eV/'+angstrom)
-sim_minimize.add(label="Force convergence criterion norm type")
-sim_minimize.add(label="Minimization scheme parameter")
-sim_minimize.add(label="Energy convergence criterion", unit='eV')
-sim_minimize.add(label="Max steps")
-sim_minimize.add(label="Max steps force evaluation")
-sim_minimize.add(label="Minimization scheme", field_type="list")
-sim_minimize.add(label="Pressure", unit="GPa")
-sim_minimize.add(label="Print detail", name="calcMinPrintDetail")
+class SimLammps(FieldList):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Potential")
+        self.add(label="Lammps version")
 
-sim_deform = FieldList()
-sim_deform.add(label="Deformation direction")
-sim_deform.add(label="Strain rate tensor")
-sim_deform.add(label="Homogeneous scaling", field_type="bool")
 
-sim_indent = FieldList()
-sim_indent.add(label="Indentor velocity", unit='m/s')
-sim_indent.add(label="Indentor direction")
-sim_indent.add(label="Indentor position")
-sim_indent.add(label="Indentor shape")  # Choice e.g. spherical
-sim_indent.add(label="Indentor size")
-sim_indent.add(label="Indentor type")  # Choice? e.g. Virtual
+class SimVasp(FieldList):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Plane wave energy cutoff", unit='eV')
+        self.add(label="Kpoint mesh")
+        self.add(label="Pseudopotential")
+        self.add(label="Spin polarization", field_type="bool")
+        # ...etc?
 
-sim_hess = FieldList()
-sim_hess.add(label="Spring constant", unit="eV/"+angstrom+"**2")
-sim_hess.add(label="Reference Job")
 
-sim_TI = FieldList()
-sim_TI.add(label="Reference A, B", name="TIRefJobs")
-sim_TI.add(label="Sample Reference", name="TISampleRef")
-sim_TI.add(label="Thermalization Steps")
-sim_TI.add(label="Number of lambda points")
-sim_TI.add(label="Custom lambda points")
-sim_TI.add(label="Statistical error tolerance", unit='eV')
+class SimMD(FieldList):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Temperature", unit='K')
+        self.add(label="Thermodynamic ensemble")
+        self.add(label="Initial temperature", unit='K')  # For initial kinetic energy
+        self.add(label="Temperature end", unit='K')  # For linear ramping between Temperature and this value
+        self.add(label="Thermostat style", field_type="list")
+        self.add(label="Thermostat timescale", unit='fs')
+        self.add(label="Pressure", unit="GPa")
+        self.add(label="Pressure end", unit="GPa")
+        self.add(label="Barostat style", field_type="list")
+        self.add(label="Barostat timescale", unit="fs")
+        self.add(label="Time step", unit='fs')
+        self.add(label="Atomic steps")
+        self.add(label="Print period")
+        self.add(label="Print detail")
+        self.add(label="Random seed")
+
+
+class SimMinimize(FieldList):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Force convergence criterion", unit='eV/'+angstrom)
+        self.add(label="Force convergence criterion norm type")
+        self.add(label="Minimization scheme parameter")
+        self.add(label="Energy convergence criterion", unit='eV')
+        self.add(label="Max steps")
+        self.add(label="Max steps force evaluation")
+        self.add(label="Minimization scheme", field_type="list")
+        self.add(label="Pressure", unit="GPa")
+        self.add(label="Print detail", name="calcMinPrintDetail")
+
+
+class SimDeform(FieldList):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Deformation direction")
+        self.add(label="Strain rate tensor")
+        self.add(label="Homogeneous scaling", field_type="bool")
+
+
+class SimIndent(FieldList):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Indentor velocity", unit='m/s')
+        self.add(label="Indentor direction")
+        self.add(label="Indentor position")
+        self.add(label="Indentor shape")  # Choice e.g. spherical
+        self.add(label="Indentor size")
+        self.add(label="Indentor type")  # Choice? e.g. Virtual
+
+
+class SimHess(FieldList):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Spring constant", unit="eV/"+angstrom+squared)
+        self.add(label="Reference Job")
+
+
+class SimTI(FieldList):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Reference A, B", name="TIRefJobs")
+        self.add(label="Sample Reference", name="TISampleRef")
+        self.add(label="Thermalization Steps")
+        self.add(label="Number of lambda points")
+        self.add(label="Custom lambda points")
+        self.add(label="Statistical error tolerance", unit='eV')
+
+
+class LammpsMin(SimMinimize, SimLammps, SimUniversal):
+    def __init__(self):
+        super().__init__()
+        self.sort_fields_by_order_priority()
+
 
 lammps_min = Scheme("AtomisticSim_LammpsMin")
-lammps_min.fields = sim_universal + sim_lammps + sim_minimize
+lammps_min.fields = LammpsMin()
 lammps_min.write()
 
 lammps_md = Scheme("AtomisticSim_LammpsMD")
-lammps_md.fields = sim_universal + sim_lammps + sim_md
+lammps_md.fields = SimUniversal() + SimLammps() + SimMD()
 lammps_md.write()
 
 lammps_deform = Scheme("AtomisticSim_LammpsDeformMD")
-lammps_deform.fields = lammps_md.fields.copy() + sim_deform
+lammps_deform.fields = lammps_md.fields.copy() + SimDeform()
 lammps_deform.write()
 
 lammps_indent = Scheme("AtomisticSim_LammpsIndentMD")
-lammps_indent.fields = lammps_md.fields.copy() + sim_indent
+lammps_indent.fields = lammps_md.fields.copy() + SimIndent()
 lammps_indent.write()
 
 vasp_min = Scheme("AtomisticSim_VaspMin")
-vasp_min.fields = sim_universal + sim_vasp + sim_minimize
+vasp_min.fields = SimUniversal() + SimVasp() + SimMinimize()
 vasp_min.write()
 
 vasp_md = Scheme("AtomisticSim_VaspMD")
-vasp_md.fields = sim_universal + sim_vasp + sim_md
+vasp_md.fields = SimUniversal() + SimVasp() + SimMD()
 vasp_md.write()
 
 scheme_name = "Atomistic_simulation"
 atomistic_simul = Scheme(scheme_name)
-atomistic_simul.fields = sim_blue
+atomistic_simul.fields = SimBasic()
 atomistic_simul.write()
 
-atomistic_simul.fields = sim_technical
+atomistic_simul.fields = SimTechnical()
 atomistic_simul.write(scheme_name+"_grey.ttl")
 
-atomistic_simul.fields = sim_vasp + sim_lammps
-atomistic_simul.fields += sim_md + sim_minimize
-atomistic_simul.fields += sim_deform + sim_indent
-atomistic_simul.fields += sim_hess + sim_TI
+atomistic_simul.fields = SimVasp() + SimLammps()
+atomistic_simul.fields += SimMD() + SimMinimize()
+atomistic_simul.fields += SimDeform() + SimIndent()
+atomistic_simul.fields += SimHess() + SimTI()
 atomistic_simul.write(scheme_name+"_green.ttl")
 
-output_blue = SFBFields()
-output_blue.add(label="Sample ID", required=True)
-output_blue.add(label="Trajectory ID")
-output_blue.add(label="Simulation ID")
-output_blue.add(label="External ID")
-output_blue.add(label="Configuration format")
-output_blue.add(label="References")
 
-output_green = FieldList()
-output_green.add("Chemical species")
-output_green.add("Number of atoms")
-output_green.add("Chemical species count")
-output_green.add("Additional properties")
-output_green.add("Timestep")
-output_green.add("Time", unit='ps')
+class AtomisticOutputCoScInE(SFBFields):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Sample ID", required=True)
+        self.add(label="Trajectory ID")
+        self.add(label="Simulation ID")
+        self.add(label="External ID")
+        self.add(label="Configuration format")
+        self.add(label="References")
+
+
+class AtomisticOutputGreen(FieldList):
+    def __init__(self):
+        super().__init__()
+        # green
+        self.add("Chemical species")
+        self.add("Number of atoms")
+        self.add("Chemical species count")
+        self.add("Additional properties")
+        self.add("Timestep")
+        self.add("Time", unit='ps')
+
+
+class AtomisticOutput(AtomisticOutputGreen, AtomisticOutputCoScInE):
+    pass
+
 
 scheme_name = "Atomistic_output"
 output = Scheme(scheme_name)
-output.fields = output_blue
+output.fields = AtomisticOutputCoScInE()
 output.write()
 
-output.fields = output_green
-output.write(scheme_name+"_green.ttl")
-
-output.fields = output_blue + output_green
+output.fields = AtomisticOutput()
 output.write(scheme_name+"_full.ttl")
 
 
-snapshot_blue = SFBFields()
-snapshot_blue.add(label="Snapshot ID")
-snapshot_blue.add(label="Sample ID")
-snapshot_blue.add(label="External ID")
-snapshot_blue.add(label="Snapshot format")
-snapshot_blue.add(label="Visualization program")
-snapshot_blue.add(label="Visualization program command")
-snapshot_blue.add(label="References")
+class AtomisticSnapshotCoScInE(SFBFields):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Snapshot ID")
+        self.add(label="Sample ID")
+        self.add(label="External ID")
+        self.add(label="Snapshot format")
+        self.add(label="Visualization program")
+        self.add(label="Visualization program command")
+        self.add(label="References")
 
-snapshot_green = FieldList()
-snapshot_green.add(label='Atomistic color')
-snapshot_green.add(label="Color information")
-snapshot_green.add(label="Perspective", field_type='bool')
+
+class AtomisticSnapshotGreen(FieldList):
+    def __init__(self):
+        super().__init__()
+        self.add(label='Atomistic color')
+        self.add(label="Color information")
+        self.add(label="Perspective", field_type='bool')
+
+
+class AtomisticSnapshot(AtomisticSnapshotGreen, AtomisticSnapshotCoScInE):
+    pass
+
 
 scheme_name = "Atomistic_snapshot"
 snapshot = Scheme(scheme_name)
-snapshot.fields = snapshot_blue
+snapshot.fields = AtomisticSnapshotCoScInE()
 snapshot.write()
 
-snapshot.fields = snapshot_green
+snapshot.fields = AtomisticSnapshotGreen()
 snapshot.write(scheme_name+"_green.ttl")
 
-snapshot.fields = snapshot_blue + snapshot_green
+snapshot.fields = AtomisticSnapshot()
 snapshot.write(scheme_name+"_full.ttl")
 
-ML_pot_blue = SFBFields()
-ML_pot_blue.add(label="Potential ID")
-ML_pot_blue.add("Potential type/ format", name="potentialID")
-ML_pot_blue.add("Software used")
-ML_pot_blue.add("References")
-# Training metadata
-ML_pot_blue.add("Parent Potential")
-ML_pot_blue.add("Training data")
-ML_pot_blue.add("Optimizer parameters")
 
-ML_pot_green = FieldList()
-ML_pot_green.add(label="Species treated")
-ML_pot_green.add(label='Structure types treated')
+class MLPotCoScInE(SFBFields):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Potential ID")
+        self.add("Potential type/ format", name="potentialID")
+        self.add("Software used")
+        self.add("References")
+        # Training metadata
+        self.add("Parent Potential")
+        self.add("Training data")
+        self.add("Optimizer parameters")
+
+
+class MLPotGreen(FieldList):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Species treated")
+        self.add(label='Structure types treated')
+
+
+class MLPot(MLPotGreen, MLPotCoScInE):
+    pass
+
 
 scheme_name = "ML_potential"
 ML_pot = Scheme(scheme_name)
-ML_pot.fields = ML_pot_blue
+ML_pot.fields = MLPotCoScInE()
 ML_pot.write()
 
-ML_pot.fields = ML_pot_green
+ML_pot.fields = MLPotGreen()
 ML_pot.write(scheme_name+"_green.ttl")
 
-ML_pot.fields = ML_pot_blue + ML_pot_green
+ML_pot.fields = MLPot()
 ML_pot.write(scheme_name+"_full.ttl")
 
