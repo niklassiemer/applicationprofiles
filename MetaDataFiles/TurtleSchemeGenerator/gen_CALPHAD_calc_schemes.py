49309e57fc1c5d0c6602da4d64ad970cf2970557
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2021-03-24 21:33:39 +0100

    Use classes in Calphad scheme generation

--- MetaDataFiles/TurtleSchemeGenerator/gen_CALPHAD_calc_schemes.py
+++ MetaDataFiles/TurtleSchemeGenerator/gen_CALPHAD_calc_schemes.py
@@ -1,25 +1,27 @@
 from data_scheme import MetaDataSchemes as Scheme
-from data_scheme import MetaDataField as Field
+from data_scheme import SFBFields
 
 micro = "\u03bc"
 deg = "\u00b0"
 
-blue = [
-    Field(label="Operator"),
-    Field(label='Affiliation'),
-    Field(label="Database"),
-    Field(label="Date", field_type='date'),
-    Field(label="Software used"),
-    Field(label="Elements selected"),
-    Field(label="Type of calculation"),
-    Field(label="Conditions", long=True),
-    Field(label="Axes used for stepping or mapping", long=True),
-    Field(label="Fixed phases"),
-    Field(label="Rejected phases"),
-    Field(label="Axes used for plotting", long=True),
-    Field(label="Comment", long=True)
-]
+
+class CalphadCalc(SFBFields):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Database")
+        self.add(label="Date", field_type='date')
+        self.add(label="Software used")
+        self.add(label="Elements selected")
+        self.add(label="Type of calculation")
+        self.add(label="Conditions", long=True)
+        self.add(label="Axes used for stepping or mapping", long=True)
+        self.add(label="Fixed phases")
+        self.add(label="Rejected phases")
+        self.add(label="Axes used for plotting", long=True)
+
+        self.sort_fields_by_order_priority()
+
 
 Calphad_calc = Scheme("Calphad_calc")
-Calphad_calc.fields = blue
+Calphad_calc.fields = CalphadCalc()
 Calphad_calc.write()
