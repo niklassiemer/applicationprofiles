bd6901c9931631d268b16124df0afa2df6f31edb
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2021-03-24 21:44:05 +0100

    Use classes in Calphad DB scheme generation

--- MetaDataFiles/TurtleSchemeGenerator/gen_CALPHAD_schemes.py
+++ MetaDataFiles/TurtleSchemeGenerator/gen_CALPHAD_schemes.py
@@ -1,45 +1,52 @@
 from data_scheme import MetaDataSchemes as Scheme
-from data_scheme import MetaDataField as Field
+from data_scheme import SFBFields, FieldList
 
 micro = "\u03bc"
 deg = "\u00b0"
 
-blue = [
-    Field(label="Creator"),
-    Field(label='Affiliation'),
-    # This will not be unique:
-    Field(label="Database filename"),
-    Field(label="Elements included"),
-    Field(label="Element data"),
-    Field(label="Date", field_type='date'),
-    Field(label="Licence"),
-    Field(label="Software used to test"),
-    Field(label="Comment", long=True),
-    Field(label="References"),
-    Field(label='DOI')
-]
 
-green = [
-    # Details of Database content
-    Field(label="Phases and models")
-]
+class CalphadDBBasic(SFBFields):
+    def __init__(self):
+        super().__init__()
+        # This will not be unique:
+        self.add(label="Database filename")
+        self.add(label="Elements included")
+        self.add(label="Element data")
+        self.add(label="Date", field_type='date')
+        self.add(label="Licence")
+        self.add(label="Software used to test")
+        # TODO: What is the purpose?
+        self.add(label="References")
 
-grey = [
-    Field(label="Crystal structure", long=True),
-    Field(label="Compounds", long=True)
-]
 
+class CalphadDBGreen(FieldList):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Phases and models")
+
+
+class CalphadDBGrey(FieldList):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Crystal structure", long=True)
+        self.add(label="Compounds", long=True)
+
+
+class CalphadDB(CalphadDBGrey, CalphadDBGreen, CalphadDBBasic):
+    def __init__(self):
+        super().__init__()
+        self.sort_fields_by_order_priority()
 
 CALPHAD_DB = Scheme("Calphad_db")
-CALPHAD_DB.fields = blue
+CALPHAD_DB.fields = CalphadDBBasic()
 CALPHAD_DB.write()
 
-CALPHAD_DB.fields = green
+CALPHAD_DB.fields = CalphadDBGreen()
 CALPHAD_DB.write("Calphad_db_green.ttl")
 
-CALPHAD_DB.fields = grey
+CALPHAD_DB.fields = CalphadDBGrey()
 CALPHAD_DB.write("Calphad_db_grey.ttl")
 
-CALPHAD_DB.fields = blue + green + grey
+CALPHAD_DB.fields = CalphadDB()
 CALPHAD_DB.write("Calphad_db_full.ttl")
 
