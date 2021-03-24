d5e5de83fa96ca3ce81e4568c6554fcc973b0163
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2021-03-24 23:13:46 +0100

    Use classes in ICP_MS scheme generation

--- MetaDataFiles/TurtleSchemeGenerator/gen_ICP_MS_schemes.py
+++ MetaDataFiles/TurtleSchemeGenerator/gen_ICP_MS_schemes.py
@@ -1,37 +1,47 @@
 from data_scheme import MetaDataSchemes as Scheme
 from data_scheme import MetaDataField as Field
+from data_scheme import SFBFields, FieldList
 
 micro = "\u03bc"
 deg = "\u00b0"
 
-blue = [
-    Field(label="Operator"),
-    Field(label="Experiment ID"),
-    Field(label="Date of preparation", field_type='date'),
-    Field(label='Sample storage'),
-    Field(label="Pre-treatment"),
-    Field(label="Analyzed elements"),
-    Field(label="Isotope"),
-    Field(label="Measurement modus"),
-    Field(label='Sweeps'),
-    Field(label="Replicates"),
-    Field(label="Instrument used")
-]
 
-yellow = [
-    Field(label="Parent sample specimen ID"),
-    Field(label="Specimen ID"),
-    Field(label="Preparation routine"),
-    Field(label="Immersion Experiment ID")
-]
+class IcpMsBasic(SFBFields):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Date of preparation", field_type='date')
+        self.add(label='Sample storage')
+        self.add(label="Pre-treatment")
+        self.add(label="Analyzed elements")
+        self.add(label="Isotope")
+        self.add(label="Measurement modus")
+        self.add(label='Sweeps')
+        self.add(label="Replicates")
+        self.add(label="Instrument used")
+
+
+class Yellow(FieldList):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Parent sample specimen ID")
+        self.add(label="Specimen ID")
+        self.add(label="Preparation routine")
+        self.add(label="Immersion Experiment ID")
+
+
+class IcpMS(Yellow, IcpMsBasic):
+    def __init__(self):
+        super().__init__()
+        self.sort_fields_by_order_priority()
+
 
 ICP_MS = Scheme("ICP_MS")
-ICP_MS.fields = blue
+ICP_MS.fields = IcpMsBasic()
 ICP_MS.write()
 
-ICP_MS.fields = yellow
+ICP_MS.fields = Yellow()
 ICP_MS.write("ICP_MS_yellow.ttl")
 
-ICP_MS.fields = blue + yellow
+ICP_MS.fields = IcpMS()
 ICP_MS.write("ICP_MS_full.ttl")
 
