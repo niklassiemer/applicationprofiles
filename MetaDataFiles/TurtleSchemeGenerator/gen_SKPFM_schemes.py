27ba70a51b9b6f02dfa429dd0abb97fd105011c1
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2021-03-24 18:12:27 +0100

    Classify SKPFM scheme

--- MetaDataFiles/TurtleSchemeGenerator/gen_SKPFM_schemes.py
+++ MetaDataFiles/TurtleSchemeGenerator/gen_SKPFM_schemes.py
@@ -6,55 +6,45 @@ from data_scheme import MetaDataSchemes as Scheme, \
 micro = "\u03bc"
 deg = "\u00b0"
 
-KPFM_dropdown = DropdownList(label="KPFM mode", options=['AM', 'FM'])
-applied_offset_V_dropdown = DropdownList(label="Applied offset voltage", options=['sample', 'tip'])
+# KPFM_dropdown = DropdownList(label="KPFM mode", options=['AM', 'FM'])
+# applied_offset_V_dropdown = DropdownList(label="Applied offset voltage", options=['sample', 'tip'])
 
-blue = [
-#    Field(label="Operator"),
-#    Field(label="Experiment ID"),
-    Field(label="Date of preparation", sh_path="dcterms:created", field_type='date'),
-    Field(label='Sample storage'),
-    Field(label="Pre-treatment"),
-    Field(label='AFM mode'),
-    Field(label="KPFM mode", field_type=KPFM_dropdown),
-    Field(label="Tip name"),
-    Field(label="Working distance", unit='nm', other_relations={"qudt:Unit": "unit:NanoM"}),
-    Field(label="Scan velocity", unit='Hz', other_relations={"qudt:Unit": "unit:HZ"}),
-    Field(label="Tip offset voltage", unit='V', other_relations={"qudt:Unit": "unit:V"}),
-    Field(label="Applied offset voltage", field_type=['sample', 'tip']),  # applied_offset_V_dropdown),
-    Field(label="Tip alternating Voltage", unit='V', other_relations={"qudt:Unit": "unit:V"}),
-    Field(label="Scan area", unit="nm x nm", other_relations={'#': "ideally this would be two float inputs",
-                                                              "#qudt:Unit": "unit:NanoM2  Does not yet exist"}),
-    Field(label="Reference"),
-    Field(label="Experiment condition", long=True),
-#    Field(label="Comments", long=True)
-]
 
-yellow = [
-    Field(label="Parent sample specimen ID"),
-    Field(label="Specimen ID"),
-    Field(label="Preparation routine"),
-    Field(label="Immersion Experiment ID")
-]
+class PreparationRoutine(SFBFields):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Parent sample specimen ID")
+        self.add(label="Specimen ID")
+        self.add(label="Preparation routine")
+        self.add(label="Immersion Experiment ID")
 
-SKPFM = Scheme("SKPFM")
-SKPFM.fields = blue
-SKPFM.write()
 
-SKPFM.fields = yellow
-SKPFM.write("SKPFM_yellow.ttl")
+class SKPFM(PreparationRoutine):
+    def __init__(self):
+        super().__init__()
+        KPFM_dropdown = DropdownList(label="KPFM mode", options=['AM', 'FM'])
+        self.add(label="Date of preparation", sh_path="dcterms:created", field_type='date')
+        self.add(label='Sample storage')
+        self.add(label="Pre-treatment")
+        self.add(label='AFM mode')
+        self.add(label="KPFM mode", field_type=KPFM_dropdown)
+        self.add(label="Tip name")
+        self.add(label="Working distance", unit='nm')
+        self.add(label="Scan velocity", unit='Hz')
+        self.add(label="Tip offset voltage", unit='V')
+        self.add(label="Applied offset voltage", field_type=['sample', 'tip'])
+        self.add(label="Tip alternating Voltage", unit='V')
+        self.add(label="Scan area", unit="nm x nm")
+        self.add(label="Reference")
+        self.add(label="Experiment condition", long=True)
 
-SKPFM.fields = blue + yellow
-SKPFM.write("SKPFM_full.ttl")
+        self.sort_fields_by_order_priority()
 
-basic_scheme = Scheme("SKPFM/basic")
-basic_scheme.fields = SFBFields()
 
-sample_origin_scheme = Scheme("SKPFM/SampleOrigin", extends=basic_scheme)
-sample_origin_scheme.fields = yellow
+SKPFM_scheme = Scheme('SKPFM')
+SKPFM_scheme.fields = SKPFM()
 
-SKPFM_scheme = Scheme("SKPFM", extends=sample_origin_scheme)
-SKPFM_scheme.fields = blue
+SKPFM_scheme.fields.write('SKPFM.txt')
 
 SKPFM_scheme.write()
 with open("SKPFM_additional_terms.ttl", 'w') as f:
