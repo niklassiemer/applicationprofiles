cd0ac39f0e4bc83c20f1686f278e654c218cf1cf
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2021-03-25 11:52:55 +0100

    move scratch

--- MetaDataFiles/TurtleSchemeGenerator/gen_scratch_schemes.py
+++ MetaDataFiles/TurtleSchemeGenerator/gen_scratch_schemes.py
@@ -1,57 +1,5 @@
 from MetaDataFiles.TurtleSchemeGenerator.data_scheme.data_scheme import MetaDataSchemes as Scheme
-from MetaDataFiles.TurtleSchemeGenerator.data_scheme.data_scheme import FieldList
-from MetaDataFiles.TurtleSchemeGenerator.fieldlist.generic import SFBFields
-
-micro = "\u03bc"
-deg = "\u00b0"
-
-
-class ScratchBasic(SFBFields):
-    def __init__(self):
-        super().__init__()
-        self.add(label="Specimen ID")
-        self.add(label="Parent sample specimen ID", name="parentSample")
-        self.add(label="Sample Location")
-        self.add(label="Instrument used", name="instrument")
-        self.add(label="Tip used", name="tip")
-        self.add(label="Scratch crystallographic orientation")
-        self.add(label="Scratch surface plane")
-        self.add(label="Loading type")
-
-
-class Grey(FieldList):
-    def __init__(self):
-        super().__init__()
-        self.add(label="Relative Humidity", unit='%', qudt="PERCENT_RH")
-        self.add(label="Temperature", unit="\u00b0C")
-        self.add(label="Environmental gas")
-        self.add(label="Test date", field_type="date")
-
-
-class Yellow(FieldList):
-    def __init__(self):
-        super().__init__()
-        self.add(label="Preparation routine", long=True)
-        self.add(label="Preparation Date", field_type="date", long=True)
-        self.add(label="Sample storage")
-
-
-class Green(FieldList):
-    def __init__(self):
-        super().__init__()
-        self.add(label="Type of test")
-        self.add(label="Scratch Length", unit=micro+"m")
-        self.add(label="Scratch Velocity", unit=micro+"m/s")
-        self.add(label="Scratch Orientation", unit=deg)
-        self.add(label="Maximum scratch load", unit='mN')
-        self.add(label="Profiling velocity", unit=micro+"m/s")
-
-
-class Scratch(Grey, Green, Yellow, ScratchBasic):
-    def __init__(self):
-        super().__init__()
-        self.sort_fields_by_order_priority()
-
+from MetaDataFiles.TurtleSchemeGenerator.fieldlist.scratch import ScratchBasic, Grey, Yellow, Green, Scratch
 
 scratch = Scheme("Scratch")
 scratch.fields = ScratchBasic()
