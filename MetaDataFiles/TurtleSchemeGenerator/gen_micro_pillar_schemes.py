41bc980cc2753de48b38ddae6564614d12105a60
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2021-03-25 07:59:57 +0100

    Use classes in MicroPillar scheme generation

--- MetaDataFiles/TurtleSchemeGenerator/gen_micro_pillar_schemes.py
+++ MetaDataFiles/TurtleSchemeGenerator/gen_micro_pillar_schemes.py
@@ -1,60 +1,73 @@
 from data_scheme import MetaDataSchemes as Scheme
-from data_scheme import MetaDataField as Field
+from data_scheme import SFBFields, FieldList
 
-micro_pillar_blue = []
-micro_pillar_blue.append(Field(label="Experiment ID", required=True))
-micro_pillar_blue.append(Field(label="Operator", required=True))
-micro_pillar_blue.append(Field(label="Specimen ID"))
-micro_pillar_blue.append(Field(label="Parent sample specimen ID", name="parentSample"))
-micro_pillar_blue.append(Field(label="Sample Location"))
-micro_pillar_blue.append(Field(label="Instrument used", name="instrument"))
-micro_pillar_blue.append(Field(label="Tip used", name="tip"))
-micro_pillar_blue.append(Field(label="Comments", long=True))
 
-micro_pillar_grey = []
-micro_pillar_grey.append(Field(label="Relative Humidity", unit='%'))
-micro_pillar_grey.append(Field(label="Environmental protection during specimen testing", name="TestingEnv"))
-micro_pillar_grey.append(Field(label="Environmental gas"))
-micro_pillar_grey.append(Field(label="Test duration"))
-micro_pillar_grey.append(Field(label="Test date", field_type="date"))
+class MicroPillarBasic(SFBFields):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Specimen ID")
+        self.add(label="Parent sample specimen ID", name="parentSample")
+        self.add(label="Sample Location")
+        self.add(label="Instrument used", name="instrument")
+        self.add(label="Tip used", name="tip")
 
-micro_pillar_yellow = []
-micro_pillar_yellow.append(Field(label="Preparation routine", long=True))
-micro_pillar_yellow.append(Field(label="Preparation Date", field_type="date", long=True))
-micro_pillar_yellow.append(Field(label="Sample storage"))
-micro_pillar_yellow.append(Field(label="Pillar Orientation", long=True))
 
-micro_pillar_green = []
-micro_pillar_green.append(Field(label="Type of test"))
-micro_pillar_green.append(Field(label="Tip ID"))
-micro_pillar_green.append(Field(label="Diamond area function"))
-micro_pillar_green.append(Field(label="Date of Calibration", field_type="date"))
-micro_pillar_green.append(Field(label="Frame stiffness", unit='N/m'))
-micro_pillar_green.append(Field(label="Target load", unit="mN"))
-micro_pillar_green.append(Field(label="Target depth", unit="nm"))
-micro_pillar_green.append(Field(label="Target strain rate", unit='/s'))
-micro_pillar_green.append(Field(label="Target loading rate", unit='mN/s'))
-micro_pillar_green.append(Field(label="Continuous stiffness measurement", field_type='bool'))
-micro_pillar_green.append(Field(label="Drift correction enabled", field_type="bool"))
-micro_pillar_green.append(Field(label="Sample temperature", unit="\u00b0C"))
-micro_pillar_green.append(Field(label="Tip temperature", unit="\u00b0C"))
+class Grey(FieldList):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Relative Humidity", unit='%', qudt='PERCENT_RH')
+        self.add(label="Environmental protection during specimen testing", name="TestingEnv")
+        self.add(label="Environmental gas")
+        self.add(label="Test duration")
+        self.add(label="Test date", field_type="date")
+
+
+class Yellow(FieldList):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Preparation routine", long=True)
+        self.add(label="Preparation Date", field_type="date", long=True)
+        self.add(label="Sample storage")
+        self.add(label="Pillar Orientation", long=True)
+
+
+class Green(FieldList):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Type of test")
+        self.add(label="Tip ID")
+        self.add(label="Diamond area function")
+        self.add(label="Date of Calibration", field_type="date")
+        self.add(label="Frame stiffness", unit='N/m')
+        self.add(label="Target load", unit="mN")
+        self.add(label="Target depth", unit="nm")
+        self.add(label="Target strain rate", unit='/s')
+        self.add(label="Target loading rate", unit='mN/s')
+        self.add(label="Continuous stiffness measurement", field_type='bool')
+        self.add(label="Drift correction enabled", field_type="bool")
+        self.add(label="Sample temperature", unit="\u00b0C")
+        self.add(label="Tip temperature", unit="\u00b0C")
+
+
+class MicroPillar(Grey, Green, Yellow, MicroPillarBasic):
+    def __init__(self):
+        super().__init__()
+        self.sort_fields_by_order_priority()
 
 
 micro_pillar = Scheme("Micropillar")
-micro_pillar.fields = micro_pillar_blue
+micro_pillar.fields = MicroPillarBasic()
 micro_pillar.write("micropillar.ttl")
 
-micro_pillar.fields = micro_pillar_grey
+micro_pillar.fields = Grey()
 micro_pillar.write('micropillar_grey.ttl')
 
-micro_pillar.fields = micro_pillar_yellow
+micro_pillar.fields = Yellow()
 micro_pillar.write('micropillar_yellow.ttl')
 
-micro_pillar.fields = micro_pillar_green
+micro_pillar.fields = Green()
 micro_pillar.write('micropillar_green.ttl')
 
-micro_pillar.fields = micro_pillar_blue + micro_pillar_grey
-micro_pillar.write('micropillar_w_gray.ttl')
 
-micro_pillar.fields = micro_pillar_blue + micro_pillar_green + micro_pillar_grey + micro_pillar_yellow
+micro_pillar.fields = MicroPillar()
 micro_pillar.write('micropillar_full.ttl')
