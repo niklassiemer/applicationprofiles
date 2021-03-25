d9dfa6c90ba37f679aa654241f8b9c4fa3272075
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2021-03-25 09:31:35 +0100

    Use classes in SVET scheme generation

--- MetaDataFiles/TurtleSchemeGenerator/gen_SVET_schemes.py
+++ MetaDataFiles/TurtleSchemeGenerator/gen_SVET_schemes.py
@@ -1,38 +1,48 @@
 from data_scheme import MetaDataSchemes as Scheme
 from data_scheme import MetaDataField as Field
+from data_scheme import SFBFields, FieldList
 
 micro = "\u03bc"
 deg = "\u00b0"
 
-blue = [
-    Field(label="Operator"),
-    Field(label="Experiment ID"),
-    Field(label="Date of preparation", field_type='date'),
-    Field(label='Sample storage'),
-    Field(label="Pre-treatment"),
-    Field(label="Reference electrode potential", unit='mV(SHE)'),
-    Field(label="Counter electrode"),
-    Field(label="Vibrating electrode"),
-    Field(label="Tip length", unit=micro+'m'),
-    Field(label="Vibrating electrode frequency", unit='Hz'),
-    Field(label="Vibrating electrode amplitude", unit='nm'),
-    Field(label="Working distance", unit=micro+'m'),
-]
 
-yellow = [
-    Field(label="Parent sample specimen ID"),
-    Field(label="Specimen ID"),
-    Field(label="Preparation routine"),
-    Field(label="Immersion Experiment ID")
-]
+class SVETBasic(SFBFields):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Date of preparation", field_type='date')
+        self.add(label='Sample storage')
+        self.add(label="Pre-treatment")
+        self.add(label="Reference electrode potential", unit='mV(SHE)', qudt='MilliV')
+        self.add(label="Counter electrode")
+        self.add(label="Vibrating electrode")
+        self.add(label="Tip length", unit=micro+'m')
+        self.add(label="Vibrating electrode frequency", unit='Hz')
+        self.add(label="Vibrating electrode amplitude", unit='nm')
+        self.add(label="Working distance", unit=micro+'m')
 
-SVET = Scheme("SVET")
-SVET.fields = blue
-SVET.write()
 
-SVET.fields = yellow
-SVET.write("SVET_yellow.ttl")
+class Yellow(FieldList):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Parent sample specimen ID")
+        self.add(label="Specimen ID")
+        self.add(label="Preparation routine")
+        self.add(label="Immersion Experiment ID")
 
-SVET.fields = blue + yellow
-SVET.write("SVET_full.ttl")
+
+class SVET(Yellow, SVETBasic):
+    def __init__(self):
+        super().__init__()
+        self.sort_fields_by_order_priority()
+
+
+SVET_scheme = Scheme("SVET")
+SVET_scheme.fields = SFBFields()
+SVET_scheme.write()
+
+SVET_scheme.fields = Yellow()
+SVET_scheme.write("SVET_yellow.ttl")
+
+SVET_scheme.fields = SVET()
+SVET_scheme.write("SVET_full.ttl")
 
