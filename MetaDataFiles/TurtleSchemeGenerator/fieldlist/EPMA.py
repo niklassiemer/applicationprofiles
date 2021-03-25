ffc30a24a1258a28daf4712b3dfd178508481616
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2021-03-25 11:40:29 +0100

    move EPMA

---
+++ MetaDataFiles/TurtleSchemeGenerator/fieldlist/EPMA.py
@@ -0,0 +1,39 @@
+from MetaDataFiles.TurtleSchemeGenerator.data_scheme.data_scheme import FieldList
+from MetaDataFiles.TurtleSchemeGenerator.data_scheme.units import deg
+from MetaDataFiles.TurtleSchemeGenerator.fieldlist.generic import SFBFields
+
+
+class EPMABasic(SFBFields):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Instrument used", name="instrument")
+        self.add(label="Specimen ID")
+        self.add(label="Parent sample specimen ID", name="parentSample")
+        self.add(label="Acquisition mode", field_type="class")
+        self.add(label="Elements included/Peak ID", name='elementsIncluded')
+
+
+class Grey(FieldList):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Count rate", unit='cps', qudt='NUM-PER-SEC')
+        self.add(label="Spot size")
+        self.add(label="Measurement time/date", name="measurementTime")
+        self.add(label="Background method")
+
+
+class Green(FieldList):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Accelerating voltage", unit="kV")
+        self.add(label="Standard used")
+        self.add(label="Magnification")
+        self.add(label="Dwell time", unit='s')
+        self.add(label="Working distance", unit='mm')
+        self.add(label="Tilt", unit=deg)
+
+
+class EPMA(Grey, Green, EPMABasic):
+    def __init__(self):
+        super().__init__()
+        self.sort_fields_by_order_priority()
--- MetaDataFiles/TurtleSchemeGenerator/gen_EPMA_schemes.py
+++ MetaDataFiles/TurtleSchemeGenerator/gen_EPMA_schemes.py
@@ -1,46 +1,5 @@
 from MetaDataFiles.TurtleSchemeGenerator.data_scheme.data_scheme import MetaDataSchemes as Scheme
-from MetaDataFiles.TurtleSchemeGenerator.data_scheme.data_scheme import FieldList
-from MetaDataFiles.TurtleSchemeGenerator.fieldlist.generic import SFBFields
-
-micro = "\u03bc"
-deg = "\u00b0"
-
-
-class EPMABasic(SFBFields):
-    def __init__(self):
-        super().__init__()
-        self.add(label="Instrument used", name="instrument")
-        self.add(label="Specimen ID")
-        self.add(label="Parent sample specimen ID", name="parentSample")
-        self.add(label="Acquisition mode", field_type="class")
-        self.add(label="Elements included/Peak ID", name='elementsIncluded')
-
-
-class Grey(FieldList):
-    def __init__(self):
-        super().__init__()
-        self.add(label="Count rate", unit='cps', qudt='NUM-PER-SEC')
-        self.add(label="Spot size")
-        self.add(label="Measurement time/date", name="measurementTime")
-        self.add(label="Background method")
-
-
-class Green(FieldList):
-    def __init__(self):
-        super().__init__()
-        self.add(label="Accelerating voltage", unit="kV")
-        self.add(label="Standard used")
-        self.add(label="Magnification")
-        self.add(label="Dwell time", unit='s')
-        self.add(label="Working distance", unit='mm')
-        self.add(label="Tilt", unit=deg)
-
-
-class EPMA(Grey, Green, EPMABasic):
-    def __init__(self):
-        super().__init__()
-        self.sort_fields_by_order_priority()
-
+from MetaDataFiles.TurtleSchemeGenerator.fieldlist.EPMA import EPMABasic, Grey, Green, EPMA
 
 EPMA_scheme = Scheme("EPMA")
 EPMA_scheme.fields = EPMABasic()
