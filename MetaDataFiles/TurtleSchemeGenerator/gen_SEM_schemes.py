787f68cfe74a2df597f252297925100a8f4bbb49
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2021-03-25 09:07:54 +0100

    Use classes in SEM scheme generation

--- MetaDataFiles/TurtleSchemeGenerator/gen_SEM_schemes.py
+++ MetaDataFiles/TurtleSchemeGenerator/gen_SEM_schemes.py
@@ -1,58 +1,67 @@
 from data_scheme import MetaDataSchemes as Scheme
-from data_scheme import MetaDataField as Field
+from data_scheme import SFBFields, FieldList
 
 micro = "\u03bc"
 deg = "\u00b0"
 
-SEM_blue = [
-    Field(label="Experiment ID", required=True),
-    Field(label="Operator", required=True),
-    Field(label="Specimen ID"),
-    Field(label="Parent sample specimen ID", name="parentSample"),
-    Field(label="Sample Location"),
-    Field(label="Instrument used", name="instrument"),
-    Field(label="Detector used", name="detector"),
-    Field(label="Comments", long=True),
-]
 
-SEM_grey = [
-    Field(label="Temperature", unit=deg+'C'),
-    Field(label="Relative Humidity", unit='%'),
-    Field(label="Environmental protection during specimen transfer", name="TestingEnv"),
-    Field(label="Environmental gas"),
-]
+class SEMBasic(SFBFields):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Specimen ID")
+        self.add(label="Parent sample specimen ID", name="parentSample")
+        self.add(label="Sample Location")
+        self.add(label="Instrument used", name="instrument")
+        self.add(label="Detector used", name="detector")
 
-SEM_yellow = [
-    Field(label="Preparation routine", long=True),
-    Field(label="Preparation Date", field_type="date", long=True),
-    Field(label="Sample storage"),
-]
 
-SEM_green = [
-    Field(label="Accelerating voltage", unit="kV"),
-    Field(label="Current", unit="mA"),
-    Field(label="Magnification"),
-    Field(label="Image width", unit=micro + "m"),
-    Field(label="Image size", unit="px(width) x px(height)"),
-    Field(label="Acquisition mode", field_type="class"),
-    Field(label="Storage Tilt", unit=deg),
-]
+class Grey(FieldList):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Temperature", unit=deg+'C'),
+        self.add(label="Relative Humidity", unit='%', qudt="PERCENT_RH"),
+        self.add(label="Environmental protection during specimen transfer", name="TestingEnv"),
+        self.add(label="Environmental gas"),
 
-SEM = Scheme("SEM")
-SEM.fields = SEM_blue
-SEM.write("SEM.ttl")
 
-SEM.fields = SEM_green
-SEM.write('SEM_green.ttl')
+class Yellow(FieldList):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Preparation routine", long=True)
+        self.add(label="Preparation Date", field_type="date", long=True)
+        self.add(label="Sample storage")
 
-SEM.fields = SEM_grey
-SEM.write('SEM_grey.ttl')
 
-SEM.fields = SEM_yellow
-SEM.write('SEM_yellow.ttl')
+class Green(FieldList):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Accelerating voltage", unit="kV")
+        self.add(label="Current", unit="mA")
+        self.add(label="Magnification")
+        self.add(label="Image width", unit=micro + "m")
+        self.add(label="Image size", unit="px(width) x px(height)", qudt="NUM")
+        self.add(label="Acquisition mode", field_type="class")
+        self.add(label="Storage Tilt", unit=deg)
 
-SEM.fields = SEM_blue + SEM_grey
-SEM.write('SEM_w_gray.ttl')
 
-SEM.fields = SEM_blue + SEM_green + SEM_grey + SEM_yellow
-SEM.write('SEM_full.ttl')
+class SEM(Grey, Green, Yellow, SEMBasic):
+    def __init__(self):
+        super().__init__()
+        self.sort_fields_by_order_priority()
+
+
+SEM_scheme = Scheme("SEM")
+SEM_scheme.fields = SEMBasic()
+SEM_scheme.write("SEM.ttl")
+
+SEM_scheme.fields = Green()
+SEM_scheme.write('SEM_green.ttl')
+
+SEM_scheme.fields = Grey()
+SEM_scheme.write('SEM_grey.ttl')
+
+SEM_scheme.fields = Yellow()
+SEM_scheme.write('SEM_yellow.ttl')
+
+SEM_scheme.fields = SEM()
+SEM_scheme.write('SEM_full.ttl')
