0f662b45cad02675cc10e054017111af17681493
Liam Huber <liam.huber@gmail.com>
2021-03-26 15:54:00 +0100

    Remove duplicate work from SKPFM scheme generation

--- MetaDataFiles/TurtleSchemeGenerator/gen_SKPFM_schemes.py
+++ MetaDataFiles/TurtleSchemeGenerator/gen_SKPFM_schemes.py
@@ -3,10 +3,4 @@ from MetaDataFiles.TurtleSchemeGenerator.fieldlist.physical.measurement.SKPFM im
 
 SKPFM_scheme = Scheme('SKPFM')
 SKPFM_scheme.fields = SKPFM()
-
-SKPFM_scheme.fields.write('SKPFM.txt')
-
-SKPFM_scheme.write()
-with open("SKPFM_additional_terms.ttl", 'w') as f:
-    for field in SKPFM_scheme.full_field_list:
-        f.write(field.ttl_term_str)
+SKPFM_scheme.fields.write('SKPFM_full.txt')
