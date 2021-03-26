54960aa93842f4f38fffc9349084b047a57a2492
Liam Huber <liam.huber@gmail.com>
2021-03-26 09:29:46 +0100

    Introduce experimental class with operator field
    To distinguish between users (who make the digital object) and operators (who did the physical work)

--- MetaDataFiles/TurtleSchemeGenerator/fieldlist/generic.py
+++ MetaDataFiles/TurtleSchemeGenerator/fieldlist/generic.py
@@ -6,8 +6,14 @@ class SFBFields(FieldList):
         super().__init__()
         self.add(label=id_name, name='ID', required=True, order_priority=1,
                  comment="ID for the object, be it sample, experiment, sim...")
-        self.add(label="Operator", required=True, order_priority=1)
+        self.add(label="User", required=True, order_priority=1)
         self.add(label="Date", required=True, order_priority=1, comment="Default is today.")
         self.add(label="Affiliation")
         self.add(label="DOIs", long=True, comment="To associate publications produced using this object.")
         self.add(label="Comments", long=True, order_priority=-1)  # Last entry in the Scheme
+
+
+class SFBExperimental(SFBFields):
+    def __init__(self):
+        super().__init__()
+        self.add(label="Operator", comment="The person who did the work. Default is the user.")
