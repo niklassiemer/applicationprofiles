1530e56d730d6457d3aebe88f43b83c2b5df0f56
Liam Huber <liam.huber@gmail.com>
2021-03-26 14:14:38 +0100

    Make date date-like

--- MetaDataFiles/TurtleSchemeGenerator/fieldlist/generic.py
+++ MetaDataFiles/TurtleSchemeGenerator/fieldlist/generic.py
@@ -7,7 +7,7 @@ class SFBFields(FieldList):
         self.add(label=id_name, name='ID', required=True, order_priority=1,
                  comment="ID for the object, be it sample, experiment, sim...")
         self.add(label="User", required=True, order_priority=1)
-        self.add(label="Date", required=True, order_priority=1, comment="Default is today.")
+        self.add(label="Date", required=True, order_priority=1, comment="Default is today.", field_type="date")
         self.add(label="Affiliation")
         self.add(label="DOIs", long=True, comment="To associate publications produced using this object.")
         self.add(label="Comments", long=True, order_priority=-1)  # Last entry in the Scheme
