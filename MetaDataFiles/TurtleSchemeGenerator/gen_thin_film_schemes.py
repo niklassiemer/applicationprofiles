a2b02505df2d581143cff331166957f2a423dc58
Liam Huber <liam.huber@gmail.com>
2021-03-26 14:01:54 +0100

    Turn thin film into a preparation routine

--- MetaDataFiles/TurtleSchemeGenerator/gen_thin_film_schemes.py
+++ MetaDataFiles/TurtleSchemeGenerator/gen_thin_film_schemes.py
@@ -1,11 +1,11 @@
 from MetaDataFiles.TurtleSchemeGenerator.data_scheme.data_scheme import MetaDataSchemes as Scheme
-from MetaDataFiles.TurtleSchemeGenerator.fieldlist.physical.measurement.thin_film import ThinFilmBasic, Grey, ThinFilm
+from MetaDataFiles.TurtleSchemeGenerator.fieldlist.physical.preparation import ThinFilmBasic, ThinFilmGrey, ThinFilm
 
 thin_film = Scheme("ThinFilm")
 thin_film.fields = ThinFilmBasic()
 thin_film.write()
 
-thin_film.fields = Grey()
+thin_film.fields = ThinFilmGrey()
 thin_film.write("ThinFilm_grey.ttl")
 
 thin_film.fields = ThinFilm()
