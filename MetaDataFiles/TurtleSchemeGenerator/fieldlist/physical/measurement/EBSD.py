2e546ae051e25f751ca2c133a138212805acacd9
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2021-04-29 19:01:27 +0200

    EBSD

--- MetaDataFiles/TurtleSchemeGenerator/fieldlist/physical/measurement/EBSD.py
+++ MetaDataFiles/TurtleSchemeGenerator/fieldlist/physical/measurement/EBSD.py
@@ -6,11 +6,12 @@ from MetaDataFiles.TurtleSchemeGenerator.fieldlist.physical.generic import Exper
 class EBSDBasic(Experiment):
     def __init__(self):
         super().__init__()
+        self.add(label="Sample location", example_input="Longitudanal cross-section; from top surface")
         self.add(label="Corrosion", field_type="bool")
-        self.add(label='Detectors used')
-        self.add(label='Location on sample')
-        self.add(label='Any data set to be linked with this experiment', long=True,
-                 comment="e.g. EBSD_scheme map etc.")
+        self.add(label='Detector ID(s)', name="detectorID")
+        self.add(label='Location on sample', example_input="5mm in X and 4 mm in Y from lower left corner")
+        # self.add(label='Any data set to be linked with this experiment', long=True,
+        #         comment="e.g. EBSD_scheme map etc.")
 
 
 class EBSDGreen(FieldList):
@@ -21,7 +22,7 @@ class EBSDGreen(FieldList):
         self.add(label="Magnification")
         self.add(label="Step size", unit=micro+"m")
         self.add(label="Raster size", unit=micro+"m(width) x "+micro+"m(height)")
-        self.add(label="Acquisition mode", field_type="class")
+        self.add(label="Acquisition mode", field_type=['Line scan', 'Zig-zag scan'])
         self.add(label="No. of points")
         self.add(label="Working distance", unit='mm')
         self.add(label="Tilt angle", unit=deg)
