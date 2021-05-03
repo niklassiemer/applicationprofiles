4518962dcad12fa72b021cd1410adce52e6abd47
Liam Huber <liam.huber@gmail.com>
2021-05-03 16:31:47 +0200

    pyiron is just a software
    and an unambiguous tag since there are multiple sub-sets of pyiron now

--- MetaDataFiles/TurtleSchemeGenerator/fieldlist/digital/generic.py
+++ MetaDataFiles/TurtleSchemeGenerator/fieldlist/digital/generic.py
@@ -23,7 +23,6 @@ class ComputeEnvironment(SFBFields):
     def __init__(self):
         super().__init__()
         self.add(label="Python version")
-        self.add(label="pyiron version")
         self.add(label="Software IDs", long=True)
 
         self.sort_fields_by_order_priority()
